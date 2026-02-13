d75016be20bde4a3dd18fe03358d8220e990f431
tonkoanastasia <tonkoanastasia@gmal.com>
2026-02-14 00:00:12 +0300

    Implement myFilter

--- task1.ts
+++ task1.ts
@@ -2,5 +2,11 @@
 // Написать метод myReduce в прототипе массива, который будет работать аналогично reduce
 
 const array: number[] = [4, 2, 3, 1];
-
-//array.myReduce((acc, value) => acc + value); //10
+Array.prototype.myReduce = function <T, R>(func: (acc: R, value: T, index: number, array: T[]) => R, initialValue?: R) {
+  let acc = initialValue ?? this[0];
+  for (let i = initialValue ? 0 : 1; i < this.length; i++) {
+    acc = func(acc, this[i], i, this);
+  }
+  return acc;
+};
+console.log(array.myReduce((acc, value) => acc + value));
